dffc2bf2088fbc493572a94c4c1aee7490563c57
Arshad Chummun <arshad@chummun.com>
2013-07-28 00:53:04 +0400

    Fixed page.tpl.php and add help region

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -3,20 +3,25 @@
     <div id="messages" class="clearfix"><?php print $messages; ?></div>
   <?php endif; ?>
   <div id="branding" class="clearfix">
-    <?php print $breadcrumb; ?>
+    <!-- <?php print $breadcrumb; ?> -->
     <?php print render($title_prefix); ?>
     <?php if ($title): ?>
       <h1 class="page-title"><?php print $title; ?></h1>
     <?php endif; ?>
     <?php print render($title_suffix); ?>
-    <?php print render($primary_local_tasks); ?>
+    <?php if ($tabs): ?>
+      <div class="tabs">
+        <?php print render($tabs); ?>
+      </div>
+    <?php endif; ?>
+    <?php if ($action_links): ?>
+      <ul class="nav nav-pills action-links">
+        <?php print render($action_links); ?>
+      </ul>
+    <?php endif; ?>
   </div>
 
   <div id="page">
-    <?php if ($secondary_local_tasks): ?>
-      <div class="tabs-secondary clearfix"><?php print render($secondary_local_tasks); ?></div>
-    <?php endif; ?>
-
     <div id="content" class="clearfix">
       <div class="element-invisible"><a id="main-content"></a></div>
       <?php if ($page['help']): ?>
